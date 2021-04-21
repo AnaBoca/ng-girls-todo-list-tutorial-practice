20b6e29898efb93a32bb851a299da2fd5f9085fa
AnaBoca <Ana.Boca@asu.edu>
2021-04-21 10:31:18 -0700

    added input-button-unit component

---
+++ todo-list/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+
+    <app-input-button-unit></app-input-button-unit>
+  `,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'todo-list';
+}
