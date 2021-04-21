f92ad4caa6c66b8f30709c1bca7f34c639b955df
AnaBoca <Ana.Boca@asu.edu>
2021-04-21 12:25:28 -0700

    new component - to do item

---
+++ todo-list/src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() item;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
