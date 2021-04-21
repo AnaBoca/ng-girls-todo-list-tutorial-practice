7a382f23abec7eb7e59a66eaba68f5bdd7aeda73
AnaBoca <Ana.Boca@asu.edu>
2021-04-21 14:45:27 -0700

    creating a service

--- todo-list/src/app/list-manager/list-manager.component.ts
+++ todo-list/src/app/list-manager/list-manager.component.ts
@@ -1,32 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import { TodoListService } from '../services/todo-list.service';
 
 @Component({
   selector: 'app-list-manager',
   template: `
-    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
+    <div class="todo-app">
+      <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
 
-    <ul>
-      <li *ngFor="let todoItem of todoList">
-        <app-todo-item [item]="todoItem"></app-todo-item>
-      </li>
-    </ul>
+      <ul>
+        <li *ngFor="let todoItem of todoList">
+          <app-todo-item [item]="todoItem"></app-todo-item>
+        </li>
+      </ul>
+    </div>
   `,
   styleUrls: ['./list-manager.component.scss']
 })
 export class ListManagerComponent implements OnInit {
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
+  todoList: TodoItem[] = [];
 
-  constructor() { }
+  constructor(private todoListService: TodoListService) { }
 
   ngOnInit(): void {
+    this.todoList = this.todoListService.getTodoList();
   }
 
   addItem(title: string): void {
---
+++ todo-list/src/app/services/todo-list.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoListService {
+  private todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
+  constructor() { }
+
+  getTodoList(): TodoItem[] {
+    return this.todoList;
+  }
+}
